3379c9b9c130b3ea17f461d43b55c9bc7fd6a0d6
Boda335 <elmassry826@gmail.com>
2026-04-28 16:41:57 +0300

    Update vidsrc.ts

--- src/vidsrc.ts
+++ src/vidsrc.ts
@@ -105,14 +105,15 @@ async function tmdbScrape(tmdbId: string, type: "movie" | "tv", season?: number,
     throw new Error("Invalid Data.");
   }
   const url = (type === "movie")
-    ? `https://vidsrc.net/embed/${type}?tmdb=${tmdbId}`
-    : `https://vidsrc.net/embed/${type}?tmdb=${tmdbId}&season=${season}&episode=${episode}`;
+    ? `https://vsembed.ru/embed/${type}?tmdb=${tmdbId}`
+    : `https://vsembed.ru/embed/${type}?tmdb=${tmdbId}&season=${season}&episode=${episode}`;
   const embed = await fetch(url);
   const embedResp = await embed.text();
 
   // get some metadata
   const { servers, title } = await serversLoad(embedResp);
-
+console.log("embedResp length:", embedResp.length);
+console.log("servers:", servers);
   const rcpFetchPromises = servers.map(element => {
     return fetch(`${BASEDOM}/rcp/${element.dataHash}`);
   });
